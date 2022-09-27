ac211fd3ad6a2c8d31a1d01e567174b491c239df
Miroboi <101923439+Miroboi@users.noreply.github.com>
2022-09-27 14:29:09 +0300

    Update en.lua

--- esx_pesuratti/locales/en.lua
+++ esx_pesuratti/locales/en.lua
@@ -1,8 +1,8 @@
 Locales ['en'] = {
-	['used_kit']			= 'Käytit ~y~x1 ~b~Pesurätin',
-	['must_be_outside']		= 'Ethän sä nyt sisältä tätä putsaa hölmö!',
-	['no_vehicle_nearby']		= 'Ei ~r~ajoneuvoa ~w~lähettyvillä',
-	['finished_repair']		= '~g~nyt muuten kimaltelee!',
-	['abort_hint']			= 'Paina ~INPUT_VEH_DUCK~ peruuttaaksesi',
-	['aborted_repair']		= 'Sinä ~r~ lopetit ~w~pesemisen',
+	['used_kit']			= 'You used ~y~x1 ~b~washing kit',
+	['must_be_outside']		= 'You can not wash your car from the inside!',
+	['no_vehicle_nearby']		= 'No ~r~vehicles ~w~nearby',
+	['finished_repair']		= '~g~Vehicle cleaned!',
+	['abort_hint']			= '~INPUT_VEH_DUCK~ Cancel',
+	['aborted_repair']		= 'You ~r~ stopped ~w~washing your car',
 }
