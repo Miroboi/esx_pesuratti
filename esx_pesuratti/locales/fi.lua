f2cc2f13e870d557e2bf0712cb841c6bb6c4fc2e
Miroboi <101923439+Miroboi@users.noreply.github.com>
2022-09-27 14:32:09 +0300

    Create fi.lua

---
+++ esx_pesuratti/locales/fi.lua
@@ -0,0 +1,8 @@
+Locales ['fi'] = {
+	['used_kit']			= 'Käytit ~y~yhden ~b~pesurätin',
+	['must_be_outside']		= 'Et voi pestä autoasti sisältä käsin!',
+	['no_vehicle_nearby']		= 'Ei ~r~ajoneuvoa ~w~lähettyvillä',
+	['finished_repair']		= '~g~Sinillä siivous sujuu!',
+	['abort_hint']			= '~INPUT_VEH_DUCK~ Peruuta',
+	['aborted_repair']		= '~r~Peruutit ~w~ ajoneuvon pesun',
+}
